374cc03b48d3aa99ce0939340a197e9a530333c2
YUNHEEJAE <d1003963@naver.com>
2017-02-01 16:56:39 +0900

    AD관리 끝

--- src/main/java/org/kb141/web/HomeController.java
+++ src/main/java/org/kb141/web/HomeController.java
@@ -1,6 +1,15 @@
 package org.kb141.web;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.kb141.domain.AdVO;
 import org.kb141.domain.ClientVO;
 import org.kb141.service.AdService;
 import org.kb141.service.ClientService;
@@ -10,13 +19,22 @@ import org.kb141.service.LogService;
 import org.kb141.service.MessageService;
 import org.kb141.util.AttributeGenerator;
 import org.kb141.util.ChartAttributes;
+import org.kb141.util.FileMaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @CrossOrigin
 @Controller
@@ -41,6 +59,8 @@ public class HomeController {
 	
 	@Autowired
 	private ClientService clientService;
+	
+
 	
 	@RequestMapping("/")
 	public String index() {
@@ -126,18 +146,126 @@ public class HomeController {
 	}
 	
 	@GetMapping("profile2")
-	public void profile2(@RequestParam("adno") Integer adno, Model model){
+	public void profile2(@RequestParam("adno") Integer adno, Model model) throws IOException{
+
 		logger.info("YHJ IS COMING");
 		logger.info("adno : " + adno);
 		model.addAttribute("adVO", adService.view(adno));
 		model.addAttribute("deviceVO", adService.getMapChecking(adno));
-		
+
 		ChartAttributes result = AttributeGenerator.
 				INSTANCE.generator(logService.getListByAdno(adno));
-		
-		System.out.println(result);
-		
 		model.addAttribute("data", result);
+	}
+	@GetMapping("profile2Register")
+	public void profile2registerGET(){
+		logger.info("profile2registerGET.......");
+	}
+	
+	@GetMapping("viewfile")
+	public @ResponseBody byte[] viewFile(String fileName) throws IOException{
+		
+		FileMaker filemaker = new FileMaker();
+		
+		return filemaker.pushFile(fileName);
+
+		
+	}
+	
+	
+	@PostMapping("profile2Register")
+	public String profile2registerPOST(@RequestParam(value="ad_image")MultipartFile imgfile,@RequestParam(value="ad_video")MultipartFile vdfile,
+									   @RequestParam(value="ad_title")String ad_title,@RequestParam(value="category")String category,
+									   @RequestParam(value="cid")String cid, @RequestParam(value="ad_content")String ad_content,
+									   @RequestParam(value="start_duration")Date start_duration,@RequestParam(value="end_duration")Date end_duration,
+									   @RequestParam(value="target_area")String target_area,@RequestParam(value="target_emotions")String target_emotions,
+									   @RequestParam(value="permission")Boolean permission,
+									   RedirectAttributes rttr){
+		
+		AdVO adVO = new AdVO();
+		FileMaker fileMaker = new FileMaker();
+		
+		fileMaker.sendFile(imgfile);
+		fileMaker.sendFile(vdfile);
+
+		String ad_image= imgfile.getOriginalFilename();
+		String ad_video = vdfile.getOriginalFilename();
+
+				adVO.setAd_content(ad_content);
+				adVO.setAd_image(ad_image);
+				adVO.setAd_title(ad_title);
+				adVO.setAd_video(ad_video);
+				adVO.setCategory(category);
+				adVO.setCid(cid);
+				adVO.setPermission(permission);
+				adVO.setStart_duration(start_duration);
+				adVO.setEnd_duration(end_duration);
+				adVO.setTarget_area(target_area);
+				adVO.setTarget_emotions(target_emotions);
+		    
+			adService.register(adVO);
+		
+		return "redirect:profile";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
+
+
+	
+	
+	@GetMapping("profile2Modify")
+	public void profile2modifyGET(@RequestParam("adno") Integer adno,Model model){
+		logger.info("AdVO ModifyGET..............");
+		model.addAttribute("adVO", adService.view(adno));
+	}
+	
+	@PostMapping("profile2Modify")
+	public String profile2ModifyPOST(@RequestParam(value="ad_image")MultipartFile imgfile,@RequestParam(value="ad_video")MultipartFile vdfile,
+			   @RequestParam(value="ad_title")String ad_title,@RequestParam(value="category")String category,
+			   @RequestParam(value="cid")String cid, @RequestParam(value="ad_content")String ad_content,
+			   @RequestParam(value="start_duration")Date start_duration,@RequestParam(value="end_duration")Date end_duration,
+			   @RequestParam(value="target_area")String target_area,@RequestParam(value="target_emotions")String target_emotions,
+			   @RequestParam(value="permission")Boolean permission,
+			   RedirectAttributes rttr){
+
+		AdVO adVO = new AdVO();
+		FileMaker fileMaker = new FileMaker();
+		
+		fileMaker.sendFile(imgfile);
+		fileMaker.sendFile(vdfile);
+		
+		String ad_image= imgfile.getOriginalFilename();
+		String ad_video = vdfile.getOriginalFilename();
+		
+		adVO.setAd_content(ad_content);
+		adVO.setAd_image(ad_image);
+		adVO.setAd_title(ad_title);
+		adVO.setAd_video(ad_video);
+		adVO.setCategory(category);
+		adVO.setCid(cid);
+		adVO.setPermission(permission);
+		adVO.setStart_duration(start_duration);
+		adVO.setEnd_duration(end_duration);
+		adVO.setTarget_area(target_area);
+		adVO.setTarget_emotions(target_emotions);
+		
+		adService.modify(adVO);
+		
+		return "redirect:profile";
+		
+	}
+	
+	@PostMapping("profile2Remove")
+	public String profile2remove(Integer adno,RedirectAttributes rttr){
+		logger.info("adService Remove..............");
+		logger.info("adService adno : " + adno);
+		adService.remove(adno);
+		rttr.addFlashAttribute("result", "success");
+		return "redirect:profile";
 	}
 	
 	
---
+++ src/main/webapp/WEB-INF/views/profile2Register.jsp
@@ -0,0 +1,139 @@
+<%@include file="header.jsp"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>  
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>  
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+            <div class="page-inner">
+                <div class="page-title">
+                    <h3>Alerts</h3>
+                    <div class="page-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="#">UI Kits</a></li>
+                            <li class="active">Alerts</li>
+                        </ol>
+                    </div>
+                </div>
+               <div id="main-wrapper">
+
+		<!-- Main content -->
+		<section class="content">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2"> 
+			<form method="post" id="inputForm" action="profile2Register"  enctype="multipart/form-data">
+				<!-- Text input-->
+				<div class="form-group">
+				  <label class=" control-label">종류</label>  
+				  <select id="category" name="category" class="form-control">
+				      <option value="A">A</option>
+				      <option value="B">B</option>
+				      <option value="C">C</option>
+				      <option value="D">D</option>
+				    </select>
+					<span class="help-block">광고의 종류를 선택하세요</span>  
+				</div>
+			
+				<div class="form-group">
+				  <label class=" control-label" >광고주아이디</label>  
+				  <input id="cid" name="cid" type="text" placeholder="광고주아이디"  class="form-control">
+					<span class="help-block">광고주의 이름을 입력하세요</span>  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >광고명</label>  
+				  <input id="ad_title" name="ad_title" type="text" placeholder="광고명"  class="form-control">
+					<span class="help-block">광고의 정식 명칭을 입력하세요</span>  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >광고 설명</label>  
+					<textarea rows="10" name="ad_content" id="ad_content" class="form-control" ></textarea>				  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >목표감정</label>  
+				  <select id="target_emotions" name="target_emotions" class="form-control">
+				      <option value="happiness">행복</option>
+				      <option value="anger">화남</option>
+				      <option value="sadness">슬픔</option>
+				      <option value="neutral">평온</option>
+				      <option value="surprise">놀람</option>
+				      <option value="fear">두려움</option>
+				      <option value="contempt">경멸</option>
+				      <option value="disgust">혐오</option>
+				      
+				    </select>
+					<span class="help-block">목표감정를 선택하세요</span>  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >목표지역</label>  
+				  <select id="target_area" name="target_area" class="form-control">
+				      <option value="seoul">서울</option>
+				      <option value="incheon">인천</option>
+				      <option value="daejeon">대전</option>
+				      <option value="daegu">대구</option>
+				      <option value="busan">부산</option>
+				      <option value="gwangju">광주</option>
+				      <option value="ulsan">울산</option>
+				      <option value="sejong">세종</option>
+				      
+				    </select>
+					<span class="help-block">목표지역를 선택하세요</span>  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >상태 코드</label>  
+				  <select id="permission" name="permission" class="form-control">
+				      <option value="false">거부</option>
+				      <option value="ture">승인</option>
+				    </select>
+					<span class="help-block">상태코드를 선택하세요</span>  
+				</div>
+				
+				<!-- Text input-->
+				<div class="form-group">
+				  <label class=" control-label">광고시작 일자</label>  
+				  <input id="start_duration" name="start_duration" type="date" placeholder="2017-01-01" class="form-control" required="" >
+				</div>
+				<!-- Text input-->
+				<div class="form-group">
+				  <label class=" control-label" >광고종료 일자</label>  
+				  <input id="end_duration" name="end_duration" type="date" placeholder="2017-01-01"  class="form-control" required="">
+				</div>
+				<div class="form-group">
+				  <label class=" control-label" >광고이미지</label>  
+				  <input id="image" type="file" placeholder="광고이미지"  class="form-control" value="ad_image" name="ad_image">
+					<span class="help-block">광고의 이미지을 넣어주세요</span>  
+				</div>
+				
+				<div class="form-group">
+				  <label class=" control-label" >광고동영상</label>  
+				  <input id="video" type="file" placeholder="광고동영상"  class="form-control" value="ad_video" name="ad_video">
+					<span class="help-block">광고의 동영상을 넣어주세요</span>  
+				</div> 
+				</form>
+				
+		<!-- 		<form method="post" id="upload" action="profile2RegisterUpload" enctype=multipart/form-data>
+				</form> -->
+				<div class="pull-right">
+				<button type="button" class="btn btn-success" id="btnRegister">Register</button>
+				<a href="list"><button type="button" class="btn btn-warning" id="btnCancel" >Cancel</button></a>
+				</div>
+				
+			</div>
+		</div>
+		</section>
+                    </div>
+            
+            <%@include file="footer.jsp"%>
+   		<script>
+   		$(document).ready(function () {
+		 	$("#btnRegister").on("click", function () {
+ 	   		$("#ad_content").val($("#ad_content").val().replace(/\n/g, "<br>"));
+ 	   		$("#inputForm").attr("method","post").submit();  
+		 	});
+   		});
+		</script>
+    </body>
+</html>
