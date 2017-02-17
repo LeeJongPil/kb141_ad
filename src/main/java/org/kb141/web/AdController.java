f0471810634126eca109d034a98e873bbd289295
monochrome123 <monochrome123@naver.com>
2017-02-17 18:45:25 +0900

    adcontroller 수정

--- src/main/java/org/kb141/web/AdController.java
+++ src/main/java/org/kb141/web/AdController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 //github.com/LeeJongPil/kb141_ad.git
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,9 +34,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * ad, device, log
  */
 
-@RestController
-@RequestMapping("/ad")
-@EnableWebSecurity
+@Controller
 @CrossOrigin
 public class AdController {
 
